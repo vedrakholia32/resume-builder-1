6db704a30ab6998063054c39c06f9ebe78487eb8
vedrakholia32 <142721772+vedrakholia32@users.noreply.github.com>
2024-04-30 18:05:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const pdf = require('html-pdf');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
